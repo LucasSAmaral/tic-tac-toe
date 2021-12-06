eb7fca5c7fcbb8ed1de36c1f7b9cd89e41084774
Lucas Amaral <lucas.samaral@b2wdigital.com>
2021-12-06 19:03:11 -0300

    Move o reducer para o arquivo de reducer

--- src/tic-tac-toe.container.tsx
+++ src/tic-tac-toe.container.tsx
@@ -2,6 +2,7 @@ import { useState, useReducer, useEffect } from "react";
 import styled from "styled-components";
 import TicTacToeSquare from "./components/tic-tac-toe-square.component";
 import { isAllTheSameValue } from "./handlers/tic-tac-toe.handlers";
+import { initialState, reducer } from "./tic-tac-toe.reducer";
 
 const positions: PossiblePosition[] = [
   "firstRow",
@@ -13,49 +14,6 @@ const positions: PossiblePosition[] = [
   "firstDiagonal",
   "secondDiagonal",
 ];
-
-const initialState: InitialStateProps = {
-  firstRow: [],
-  secondRow: [],
-  thirdRow: [],
-  firstColumn: [],
-  secondColumn: [],
-  thirdColumn: [],
-  firstDiagonal: [],
-  secondDiagonal: [],
-};
-
-const reducer = (state: StateProps, action: ActionType): StateProps => {
-  switch (action.type) {
-    case "firstRow":
-      return { ...state, firstRow: [...state.firstRow, action.payload] };
-    case "secondRow":
-      return { ...state, secondRow: [...state.secondRow, action.payload] };
-    case "thirdRow":
-      return { ...state, thirdRow: [...state.thirdRow, action.payload] };
-    case "firstColumn":
-      return { ...state, firstColumn: [...state.firstColumn, action.payload] };
-    case "secondColumn":
-      return {
-        ...state,
-        secondColumn: [...state.secondColumn, action.payload],
-      };
-    case "thirdColumn":
-      return { ...state, thirdColumn: [...state.thirdColumn, action.payload] };
-    case "firstDiagonal":
-      return {
-        ...state,
-        firstDiagonal: [...state.firstDiagonal, action.payload],
-      };
-    case "secondDiagonal":
-      return {
-        ...state,
-        secondDiagonal: [...state.secondDiagonal, action.payload],
-      };
-    default:
-      return { ...state };
-  }
-};
 
 const TicTacToe: React.FC = () => {
   const [turn, setTurn] = useState<Turn>("X");
---
+++ src/tic-tac-toe.reducer.ts
@@ -0,0 +1,42 @@
+export const initialState: InitialStateProps = {
+    firstRow: [],
+    secondRow: [],
+    thirdRow: [],
+    firstColumn: [],
+    secondColumn: [],
+    thirdColumn: [],
+    firstDiagonal: [],
+    secondDiagonal: [],
+  };
+
+export const reducer = (state: StateProps, action: ActionType): StateProps => {
+    switch (action.type) {
+      case "firstRow":
+        return { ...state, firstRow: [...state.firstRow, action.payload] };
+      case "secondRow":
+        return { ...state, secondRow: [...state.secondRow, action.payload] };
+      case "thirdRow":
+        return { ...state, thirdRow: [...state.thirdRow, action.payload] };
+      case "firstColumn":
+        return { ...state, firstColumn: [...state.firstColumn, action.payload] };
+      case "secondColumn":
+        return {
+          ...state,
+          secondColumn: [...state.secondColumn, action.payload],
+        };
+      case "thirdColumn":
+        return { ...state, thirdColumn: [...state.thirdColumn, action.payload] };
+      case "firstDiagonal":
+        return {
+          ...state,
+          firstDiagonal: [...state.firstDiagonal, action.payload],
+        };
+      case "secondDiagonal":
+        return {
+          ...state,
+          secondDiagonal: [...state.secondDiagonal, action.payload],
+        };
+      default:
+        return { ...state };
+    }
+  };
